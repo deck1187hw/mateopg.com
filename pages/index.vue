1ca4017630e8158ac53cd36c3c1b8e91c48a5c83
miguelpuiggarcia <miguelpg@nandos.co.uk>
2020-04-05 18:08:35 +0100

    changed logo

--- pages/index.vue
+++ pages/index.vue
@@ -3,27 +3,9 @@
     <div>
       <logo />
       <h1 class="title">
-        mateopg
+        Mi cerde
       </h1>
-      <h2 class="subtitle">
-        Blog by Mateo Puig
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      
     </div>
   </div>
 </template>
